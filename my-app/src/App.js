76ba1b2ba27112b664bb4ea71f5f0ce8f593b3a1
Joel <leojmaro@gmail.com>
2024-05-23 21:54:10 -0230

    initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,46 @@
+
+import './App.css';
+import React, {useState} from 'react';
+import MovieData from './movies.json';
+import {Button} from 'react-bootstrap'
+// import {Button, Navbar} from 'react-bootstrap'
+
+
+const Movie = ({ name, year, rating }) => {
+	return (
+		<div>
+			<h2>{name}</h2>
+			<p>Year: {year}</p>
+			<p>Rating: {rating}</p>
+		</div>
+	);
+};
+
+function MovieList({movieData}) {
+	return (
+		<>
+			{movieData.map(({name, year, rating}) => {
+				return <Movie key={`${name}-${year}`} name={name} year={year} rating={rating} />;
+			})}
+		</>
+	);
+}
+
+function NavBar({isLoggedIn, onLoginToggleClick}){
+	return(
+	<Button variant="success" onClick={onLoginToggleClick}>
+		{isLoggedIn ? 'Logout' : 'Login'}
+	</Button>
+	);
+}
+
+export default function App() {
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
+	const loginToggle = () => setIsLoggedIn(prevState => !prevState);
+	return (
+		<>
+			<NavBar onLoginToggleClick={loginToggle} isLoggedIn={isLoggedIn}  />
+			<MovieList movieData={MovieData.movies}/>
+		</>
+	);
+}
